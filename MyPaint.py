0b1e0c615915d3218eb45be03307a8016fdfcd4a
tassoeb <tasso_barbosa@yahoo.com.br>
2017-01-05 21:07:08 -0200

    Top bar included, starting tools

--- MyPaint.py
+++ MyPaint.py
@@ -2,9 +2,35 @@ from Tkinter import *
 
 class TopBar:
 	def __init__(self, root):
-		pass
+		self.root = root
+		self.menu = Menu(root)
+		self.menuFile = Menu(self.menu)
+		self.menuEdit = Menu(self.menu)
+		self.menuView = Menu(self.menu)
+		self.menuImage = Menu(self.menu)
+		self.menuOptions = Menu(self.menu)
+		self.menuHelp = Menu(self.menu)
+		self.menu.add_cascade(label = "File", menu = self.menuFile)
+		self.menu.add_cascade(label = "Edit", menu = self.menuEdit)
+		self.menu.add_cascade(label = "View", menu = self.menuView)
+		self.menu.add_cascade(label = "Image", menu = self.menuImage)
+		self.menu.add_cascade(label = "Options", menu = self.menuOptions)
+		self.menu.add_cascade(label = "Help", menu = self.menuHelp)
+
+  		self.menuFile.add_command(label="New")
+		self.menuFile.add_command(label="Open")
+		self.menuFile.add_command(label="Save")
+		self.root.configure(menu = self.menu)
+
+class Tools:
+	def __init__(self,root):
+		self.root = root
+		self.frame = Frame(root)
+		
 
 root = Tk()
 root.title('MyPaint')
+root.geometry("400x300")
 bar = TopBar(root)
+tools = Tools(root)
 root.mainloop()
